0c99163a60357f192e820409a7e3a635bee7dfca
React Lab Files <95652257+reactfiles@users.noreply.github.com>
2023-08-08 11:56:46 +0530

    Update step6-express.js

--- day2/step6-express.js
+++ day2/step6-express.js
@@ -10,3 +10,26 @@ app.listen(5050,"localhost",function(error){
         console.log("Server is now live on localhost:5050");
     }
 });
+
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+const express = require("express");
+let app = express();
+app.get("/", function(req, res){
+    res.send("Hello from express")
+})
+app.get("/:hsbc", function(req, res){
+    console.log("you said", req.params.hsbc);
+    // res.end();
+    res.redirect("/");
+})
+app.listen(5050,"localhost",function(error){
+    if(error){
+        console.log("Error ", error);
+    }else{
+        console.log("Server is now live on localhost:5050");
+    }
+});
